a1d159091f551bd3b578cd6dfcfcc870a0894797
Jakub Sornat <jakubsornat2001@gmail.com>
2024-12-09 22:10:10 +0100

    delete

--- EnglishPremierLeague.py
+++ EnglishPremierLeague.py
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-
-
 import kaggle
 
 
@@ -18,13 +15,3 @@ output_dir = "data"
 api.dataset_download_files(dataset, path=output_dir, unzip=True)
 
 print(f"Zestaw danych został pobrany do katalogu: {output_dir}")
-=======
-import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import plotly.express as px
-import streamlit as st
-import pandas as pd
-
->>>>>>> 2b291fd (new structure)
