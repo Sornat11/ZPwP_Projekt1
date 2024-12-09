aa809279846b2ab1598afd790572b4e09581957e
Filip <fkopanko@gmail.com>
2024-12-09 21:43:21 +0100

    prosze

---
+++ EnglishPremierLeague.py
@@ -0,0 +1,20 @@
+pip install kaggle
+
+import kaggle
+
+# Sprawdzenie autoryzacji
+!kaggle datasets list
+
+import os
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+# Inicjalizacja API
+api = KaggleApi()
+api.authenticate()
+
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("saife245/english-premier-league")
+
+print("Path to dataset files:", path)
